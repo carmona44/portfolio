20475d418219ef948cd05f0fcc8b7cb6e22d9d21
carmona44 <daniel_95ca@hotmail.com>
2019-02-26 20:57:55 +0100

    Redaccion apartado Sobre mi

--- src/Contenido.js
+++ src/Contenido.js
@@ -3,18 +3,43 @@ import './Contenido.css';
 
 class Contenido extends Component {
     render(){
-        let repetir = [];
-
-        function repeticiones(){
-            for (var i=0; i<50; i++){
-                repetir.push(i);
-            }
-        }
-
-        repeticiones();
         return (
             <div className="contenido">
-                {repetir.map(e => <h1>e</h1>)}
+                <div>
+                    <h2>SOBRE MI</h2>
+                    <p>
+                        Soy Daniel Carmona Alarcón, programador junior y dearrollador de aplicaciones multiplataforma. Me defino como
+                        una persona calmada pero inquieta a la que le gusta aprender constantemente. Me encanta el deporte, la naturaleza,
+                        la ciencia y sobre todo, la tecnología.
+                    </p>
+                    <p>
+                        Mi principal objetivo es hacer de este un mundo mejor, por ello, ví en la programación una buena herramienta para
+                        resolver problemas y ayudar a los demás.
+                    </p>
+                    <p>
+                        Entre mis conocimientos destacan:
+                        <ul>
+                            <li>CSS3</li>
+                            <li>HTML5</li>
+                            <li>JavaScript</li>
+                            <li>Java</li>
+                            <li>AngularJS</li>
+                            <li>ReactJS</li>
+                            <li>NodeJS</li>
+                            <li>SQL</li>
+                        </ul>
+                        Siendo JavaScript junto con ReactJS el lenguaje y framework que utilizo actualmente en mis proyectos.
+                    </p>
+                </div>
+                <div>
+                    <h2>PROYECTOS</h2>
+                </div>
+                <div>
+                    <h2>CONTACTO</h2>
+                </div>
+                <div>
+                    <h2>BLOG</h2>
+                </div>
             </div>
         );
     }
