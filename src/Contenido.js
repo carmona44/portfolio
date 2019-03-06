ae14a6d35a5de3889227dac930d18263535ad983
carmona44 <daniel_95ca@hotmail.com>
2019-03-06 20:28:04 +0100

    Parallax, Blog, Contacto + CSS

--- src/Contenido.js
+++ src/Contenido.js
@@ -22,30 +22,36 @@ class Contenido extends Component {
             <div className="contenido">
                 <div id="sobremi">
                     <h2>SOBRE MI</h2>
-                    <p>
-                        Soy Daniel Carmona Alarcón, programador junior y desarrollador de aplicaciones multiplataforma. Me defino como
-                        una persona calmada pero inquieta a la que le gusta aprender constantemente. Me encanta el deporte, la naturaleza,
-                        la ciencia y sobre todo, la tecnología.
-                    </p>
-                    <p>
-                        Mi principal objetivo es hacer de este un mundo mejor, por ello, ví en la programación una buena herramienta para
-                        resolver problemas y ayudar a los demás.
-                    </p>
-                    <p>
-                        Entre mis conocimientos destacan:
-                        <ul className="conocimientos">
-                            <li><i class="fab fa-css3-alt"></i> CSS3</li>
-                            <li><i class="fab fa-html5"></i> HTML5</li>
-                            <li><i class="fab fa-js-square"></i> JavaScript</li>
-                            <li><i class="fab fa-java"></i> Java</li>
-                            <li><i class="fab fa-angular"></i> AngularJS</li>
-                            <li><img src="https://ionicframework.com/img/meta/favicon-16x16.png"></img> Ionic</li>
-                            <li><i class="fab fa-react"></i> ReactJS</li>
-                            <li><i class="fab fa-node"></i> NodeJS</li>
-                            <li><i class="fas fa-database"></i>SQL</li>
-                        </ul>
-                        Siendo JavaScript junto con ReactJS el lenguaje y framework que utilizo actualmente en mis proyectos.
-                    </p>
+                    <div className="contenidoSobremi">
+                        <p>
+                            Soy <span className="palabraDestacada">Daniel Carmona Alarcón</span>, programador junior y
+                            desarrollador de aplicaciones multiplataforma. Me defino como
+                            una persona calmada pero inquieta a la que le gusta aprender constantemente.
+                            Me encanta el deporte, la naturaleza,
+                            la ciencia y sobre todo, la tecnología.
+                        </p>
+                        <p>
+                            Mi <span className="palabraDestacada">principal objetivo</span> es hacer de este un mundo mejor,
+                            por ello, ví en la programación una buena herramienta para
+                            resolver problemas y <span className="palabraDestacada">ayudar a los demás</span>.
+                        </p>
+                        <p>
+                            Entre mis conocimientos destacan:
+                            <ul className="conocimientos">
+                                <li><i class="fab fa-css3-alt"></i> CSS3</li>
+                                <li><i class="fab fa-html5"></i> HTML5</li>
+                                <li><i class="fab fa-js-square"></i> JavaScript</li>
+                                <li><i class="fab fa-java"></i> Java</li>
+                                <li><i class="fab fa-angular"></i> AngularJS</li>
+                                <li><img src="https://ionicframework.com/img/meta/favicon-16x16.png"></img> Ionic</li>
+                                <li><i class="fab fa-react"></i> ReactJS</li>
+                                <li><i class="fab fa-node"></i> NodeJS</li>
+                                <li><i class="fas fa-database"></i>SQL</li>
+                            </ul>
+                            Siendo <span className="palabraDestacada">JavaScript</span> junto con <span className="palabraDestacada">
+                            ReactJS</span> el lenguaje y framework que utilizo actualmente en mis proyectos.
+                        </p>
+                    </div>
                 </div>
                 <div id="proyectos">
                     <h2>PROYECTOS</h2>
@@ -55,9 +61,28 @@ class Contenido extends Component {
                 </div>
                 <div id="contacto">
                     <h2>CONTACTO</h2>
+                    <div className="contenidoContacto">
+                        <a className="botonContenido" href="mailto:dcacarmona44@gmail.com"><i class="far fa-envelope"></i> Mándame un correo</a><i id="copiarCorreo" class="far fa-clone" title="Copiar correo al portapapeles"></i>
+                        <p>O contacta conmigo a través de: </p>
+                        <div className="iconosContacto">
+                            <a href="https://twitter.com/Carmona44" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                            <a href="https://www.facebook.com/daniel.carmonaalarcon" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                            <a href="https://www.instagram.com/carmona44/" target="_blank"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.strava.com/athletes/carmonaentrenos" target="_blank"><i class="fab fa-strava"></i></a>
+                        </div>
+                    </div>
                 </div>
                 <div id="blog">
                     <h2>BLOG</h2>
+                    <div className="contenidoBlog">
+                        <p className="parrafoBlog">
+                            Escribir y plasmar algunos de mis pensamientos siempre ha sido algo que me ha gustado. Por ello realizar un
+                            BLOG es uno de mis proyectos venideros y que más pronto que tarde comenzaré a desarrollar. Mientras
+                            tanto puedes echarle un vistazo a uno antiguo que con tanto cariño me gusta recordar de vez en cuando...
+                        </p>
+                        <a className="botonContenido" href="https://carmona44web.wordpress.com/" target="_blank">
+                            <i class="fas fa-angle-right"></i> Ir al BLOG antiguo</a>
+                    </div>
                 </div>
             </div>
         );
--- src/Perfil.js
+++ src/Perfil.js
@@ -10,11 +10,12 @@ class Perfil extends Component{
             <div className="perfil">
                 <img src={foto} className="foto" alt="Foto de perfil"/>
                 <h3>Daniel Carmona</h3>
-                <div>
-                    <div><a href="https://github.com/carmona44"><i class="fab fa-github"></i> GitHub</a></div>
-                    <div><a href="https://twitter.com/Carmona44?lang=es"><i class="fab fa-twitter" style={{color: '#1da1f2'}}></i> Twitter</a></div>
-                    <div><a href="mailto:dcacarmona44@gmail.com"><i class="far fa-envelope"></i> Correo</a></div>
-                    <div><a href="https://www.strava.com/athletes/carmonaentrenos"><i class="fab fa-strava" style={{color: "#fc4c02"}}></i> Strava</a></div>
+                <p className="descripcion"> Programador | Deportista</p>
+                <div className="iconosPerfil">
+                    <div><a href="https://github.com/carmona44"><i class="fab fa-github" style={{color: 'white'}}></i></a></div>
+                    <div><a href="https://twitter.com/Carmona44?lang=es"><i class="fab fa-twitter" style={{color: '#1da1f2'}}></i></a></div>
+                    <div><a href="mailto:dcacarmona44@gmail.com"><i class="far fa-envelope" style={{color: 'white'}}></i></a></div>
+                    <div><a href="https://www.strava.com/athletes/carmonaentrenos"><i class="fab fa-strava" style={{color: "#fc4c02"}}></i></a></div>
                 </div>
                 <div className="footer">
                     &copy; {year} Daniel Carmona
