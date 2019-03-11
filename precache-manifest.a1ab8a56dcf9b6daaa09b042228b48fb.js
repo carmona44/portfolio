2b31201c781cdb6304962746fd9ee9f587e6b4b9
carmona44 <daniel_95ca@hotmail.com>
2019-03-11 14:50:40 +0100

    Updates

---
+++ precache-manifest.a1ab8a56dcf9b6daaa09b042228b48fb.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = [
+  {
+    "revision": "214be83dc5e681d0e4f8ceb33dd0ea3c",
+    "url": "/portfolio/static/media/ARESSENCE.214be83d.ttf"
+  },
+  {
+    "revision": "79ab0681640a936eb031",
+    "url": "/portfolio/static/css/main.937ed118.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "c931c18a5a38dee30b03",
+    "url": "/portfolio/static/js/2.c931c18a.chunk.js"
+  },
+  {
+    "revision": "70f4fa18717415b66de206f8010bd03b",
+    "url": "/portfolio/static/media/foto-perfil.70f4fa18.jpeg"
+  },
+  {
+    "revision": "9c9409b4ab8580f04e4e387fc8919921",
+    "url": "/portfolio/static/media/webs-interesantes.9c9409b4.JPG"
+  },
+  {
+    "revision": "97a32fda8ec538a2ee702294260184bf",
+    "url": "/portfolio/static/media/markdown-previewer.97a32fda.JPG"
+  },
+  {
+    "revision": "a48990e8d64debecf740bd50c41176c0",
+    "url": "/portfolio/static/media/calculadora.a48990e8.JPG"
+  },
+  {
+    "revision": "11c5cd970ab2c973986a61fbd2dbbe8f",
+    "url": "/portfolio/static/media/generador-password.11c5cd97.JPG"
+  },
+  {
+    "revision": "cfb23cf74f2cfedd3a886645ef7f7a8c",
+    "url": "/portfolio/static/media/random-team.cfb23cf7.JPG"
+  },
+  {
+    "revision": "bca7304a18f9c67d8c0211655e113a41",
+    "url": "/portfolio/static/media/portfolio.bca7304a.JPG"
+  },
+  {
+    "revision": "d60eb49091efb9d6b52d6c50d0f98b5a",
+    "url": "/portfolio/static/media/ARESSENCE.d60eb490.woff"
+  },
+  {
+    "revision": "0026b3f9f25372cc4d4dd0070f7a6e4d",
+    "url": "/portfolio/static/media/COPRGTL.0026b3f9.woff"
+  },
+  {
+    "revision": "a2ef565db9e65768f8dd6e73e85b6ec5",
+    "url": "/portfolio/static/media/COPRGTL.a2ef565d.eot"
+  },
+  {
+    "revision": "4e9a36a2c68bcfbe6ee3ffee2ef8027e",
+    "url": "/portfolio/static/media/COPRGTL.4e9a36a2.ttf"
+  },
+  {
+    "revision": "f27ce837efb9181e3ff7bd56f0b7ae33",
+    "url": "/portfolio/static/media/ARESSENCE.f27ce837.eot"
+  },
+  {
+    "revision": "79ab0681640a936eb031",
+    "url": "/portfolio/static/js/main.79ab0681.chunk.js"
+  },
+  {
+    "revision": "ca11326f55239755ba39f3caabe00048",
+    "url": "/portfolio/static/media/PRISTINA.ca11326f.woff"
+  },
+  {
+    "revision": "3e9109c55737f38d095f16080c5f81da",
+    "url": "/portfolio/static/media/PRISTINA.3e9109c5.eot"
+  },
+  {
+    "revision": "67b76dc0172e6d8ff94b2c3f7f36c92f",
+    "url": "/portfolio/static/media/PRISTINA.67b76dc0.ttf"
+  },
+  {
+    "revision": "40670ec8d9c46a918adb0cebe6baa242",
+    "url": "/portfolio/static/media/ARESSENCE.40670ec8.svg"
+  },
+  {
+    "revision": "b61aa37d6b82cd513087409aae5b8f37",
+    "url": "/portfolio/static/media/COPRGTL.b61aa37d.svg"
+  },
+  {
+    "revision": "2528fed4bcd0771e69839fa500264245",
+    "url": "/portfolio/static/media/PRISTINA.2528fed4.svg"
+  },
+  {
+    "revision": "95e2ef430ad2b16edd331dc576e47c7f",
+    "url": "/portfolio/static/media/ARJULIAN.95e2ef43.woff"
+  },
+  {
+    "revision": "efc0b1a21c5773f1aae2ad520536a669",
+    "url": "/portfolio/static/media/ARJULIAN.efc0b1a2.eot"
+  },
+  {
+    "revision": "6cc607221d785e37c3393817273a9867",
+    "url": "/portfolio/static/media/ARJULIAN.6cc60722.ttf"
+  },
+  {
+    "revision": "10eff55c14053ea4e2359e6d375a790a",
+    "url": "/portfolio/static/media/BRLNSR.10eff55c.woff"
+  },
+  {
+    "revision": "9613f1829f310d89c2384b8666be537d",
+    "url": "/portfolio/static/media/BRLNSR.9613f182.eot"
+  },
+  {
+    "revision": "fe2027c27b6a24505f548c6fd2e1076d",
+    "url": "/portfolio/static/media/BRLNSR.fe2027c2.ttf"
+  },
+  {
+    "revision": "802fd148b2f781148fd53033c0cb2d3d",
+    "url": "/portfolio/static/media/binary-earth.802fd148.jpg"
+  },
+  {
+    "revision": "ec83f1055a5d4f7624fce8754ab38b55",
+    "url": "/portfolio/static/media/ARJULIAN.ec83f105.svg"
+  },
+  {
+    "revision": "444ac6f703a73483d463a6193b219ecf",
+    "url": "/portfolio/static/media/BRLNSR.444ac6f7.svg"
+  },
+  {
+    "revision": "366cb4ec5d9991d7cebf391638e41b1a",
+    "url": "/portfolio/index.html"
+  }
+];
